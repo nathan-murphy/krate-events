3555807873400cdc5edb90e564bb3734e1d44485
Nathan Murphy <10726539+nathan-murphy@users.noreply.github.com>
2024-06-23 15:40:39 -0500

    adding UI elements for RSVP delegation

--- client/src/app/potluck-rsvp/potluck-rsvp-edit/potluck-rsvp-edit.dialog.ts
+++ client/src/app/potluck-rsvp/potluck-rsvp-edit/potluck-rsvp-edit.dialog.ts
@@ -3,6 +3,8 @@ import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { FormRecord, FormControl, Validators } from "@angular/forms";
 import { PotluckRSVP } from "../potluck-rsvp.model";
 import { PotluckRSVPService } from "../potluck-rsvp.service";
+import { UserService } from "src/app/users/user.service";
+import { User } from "src/app/users/user.model";
 
 @Component({
   selector: "app-potluck-rsvp-edit",
@@ -12,11 +14,15 @@ import { PotluckRSVPService } from "../potluck-rsvp.service";
 export class PotluckRSVPEditDialog implements OnInit {
   public rsvpForm: FormRecord;
   private recipeText: string = "";
+  private additionalRecipeText: string = "";
+  private user: User;
+  additionalUser: User;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public potluckId: string,
-    private potluckRsvpService: PotluckRSVPService
-  ) {}
+    private potluckRsvpService: PotluckRSVPService,
+    private userService: UserService
+  ) { }
 
   onSubmitRsvp() {
     const rsvp: PotluckRSVP = {
@@ -29,12 +35,37 @@ export class PotluckRSVPEditDialog implements OnInit {
   }
 
   ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe((user) => {
+      this.user = user
+      const userCanRSVPFor: string = this.user.permissions.canRSVPFor;
+      if (userCanRSVPFor != this.user._id)
+        this.userService.getUser(userCanRSVPFor).subscribe(user => this.additionalUser = user)
+    });
+
     this.rsvpForm = new FormRecord({});
-    this.rsvpForm.addControl("rsvp", new FormControl("", Validators.required));
+    this.rsvpForm.addControl(
+      "rsvp",
+      new FormControl("", Validators.required)
+    );
     this.rsvpForm.addControl(
       "recipe",
       new FormControl("", Validators.required)
     );
+    this.rsvpForm.addControl(
+      "agree-to-rsvp",
+      new FormControl("",)
+    );
+    this.rsvpForm.addControl(
+      "rsvp-additional",
+      new FormControl("", Validators.required)
+    );
+    this.rsvpForm.addControl(
+      "recipe-additional",
+      new FormControl("", Validators.required)
+    );
+
+
+
     this.rsvpForm.valueChanges.subscribe((value) => {
       if (value["rsvp"] == "no") {
         this.recipeText = value["recipe"];
@@ -43,6 +74,16 @@ export class PotluckRSVPEditDialog implements OnInit {
         this.rsvpForm.addControl(
           "recipe",
           new FormControl(this.recipeText, Validators.required),
+          { emitEvent: false }
+        );
+      }
+      if (value["rsvp-additional"] == "no") {
+        this.additionalRecipeText = value["recipe-additional"];
+        this.rsvpForm.removeControl("recipe-additional", { emitEvent: false });
+      } else {
+        this.rsvpForm.addControl(
+          "recipe-additional",
+          new FormControl(this.additionalRecipeText, Validators.required),
           { emitEvent: false }
         );
       }
